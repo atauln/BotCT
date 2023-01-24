eb5e0ece0ed48eb20d2cbc2757ac4ef422c5e7c6
atauln <ataulnoor75@gmail.com>
2023-01-24 09:39:50 -0500

    Updated websocket connection on backend

--- server/request.py
+++ server/request.py
@@ -14,13 +14,16 @@ async def handler(websocket):
         except websockets.ConnectionClosedOK:
             break
         match message['event']:
+            print("received message")
             case "gunshot":
                 song = AudioSegment.from_wav("gunshot.wav")
                 play(song)
 
 
 async def main():
+    print("websocket opening...")
     async with websockets.serve(handler, "0.0.0.0", 8001):
+        print("websocket is now open, hosted on 0.0.0.0:8001")
         await asyncio.Future()
 
 if __name__ == "__main__":
--- site/app.py
+++ site/app.py
@@ -1,9 +1,19 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import os
+import asyncio
+import websockets
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('index.html', url=os.environ['CONN_URL'], port=os.environ['CONN_PORT'])
+    return render_template('index.html', url=os.environ['CONN_URL'], port=os.environ['CONN_PORT'], protocol=os.environ['PROTOCOL'])
+
+@app.route('/play')
+async def play():
+    async with websockets.connect('{os.environ["PROTOCOL"]}://{os.environ["CONN_URL"]}:{os.environ["CONN_PORT"]}') as websocket:
+        await websocket.send('{"event":"gunshot"}')
+        await websocket.close()
+    return redirect('/')
